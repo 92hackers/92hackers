97f78a0a665ea43091eade71525af91f97258308
ChenYuan <13608569225@163.com>
2015-12-14 19:25:01 +0800

    add GlobalObject, which is a website level namespace.

---
+++ client/lib/global/GlobalObject.js
@@ -0,0 +1,14 @@
+/**
+ *  this file defines the website level variable GlobalObject.
+ * Created by cy on 14/12/15.
+ */
+
+    // website level unique namespace, if you need some data to be
+    // used across files, just attach the data to this object.
+
+GlobalObject = {};
+
+GlobalObject.website = {
+    version: "0.0.1",
+    founder: "ChenYuan"
+};
---
+++ client/templates/global/head.js
@@ -0,0 +1,7 @@
+/**
+ *      the file is used to handle the contents inside of <head> tag,
+ *      which are meta data of web page.
+ * Created by cy on 14/12/15.
+ */
+
+// todo: the title of the page should be changed with different templates.
